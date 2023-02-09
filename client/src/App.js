9ecb7c11990c05ccd15fcd1a7812c26488f218cf
Tobias Blaksley Iraola <tblanksley@beru.io>
2023-02-09 14:51:58 -0300

    Login la concha de tu madre

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import LandingPage from "./components/LandingPage/LandingPage";
 import Destination from "./components/Destination/Destination";
 import CardDetail from "./components/CardDetail/CardDetail";
 import About from "./components/About/About";
-import SingUp from "./components/Login/Login";
+import Login from "./components/Login/Login";
 
 function App() {
 
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" exact={true} element={<LandingPage />} />
         <Route path="/destination" exact={true} element={<Destination />} />
         <Route path="/about" exact={true} element={<About />} />
-        <Route path="/login" exact={true} element={<SingUp />} />
+        <Route path="/login" exact={true} element={<Login />} />
         <Route path="/destination/:id" element={<CardDetail />} />
 
       </Routes>
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,14 +1,15 @@
 import {
     FormControl, FormLabel, FormErrorMessage, FormHelperText, Input,
-    Stack, Button, Select, WrapItem, Card, CardBody, Image,
+    Stack, Button, Select, Card, CardBody, Image,
     Heading, Text, Divider
 } from '@chakra-ui/react';
 import { useState } from "react";
-import NavBar from "../NavBar/NavBar";
 import "./Login.css";
 import { useAuth0 } from "@auth0/auth0-react";
+import { FacebookLoginButton, GoogleLoginButton } from 'react-social-login-buttons';
+import NavBar from '../NavBar/NavBar';
 
-function SingUp(props) {
+function Login(props) {
 
     const Users = [];
     const PaisesArray = ["Afganistán", "Albania", "Alemania", "Andorra", "Angola", "Antigua y Barbuda", "Arabia Saudita", "Argelia", "Argentina", "Armenia", "Australia", "Austria", "Azerbaiyán", "Bahamas", "Bangladés", "Barbados", "Baréin", "Bélgica", "Belice", "Benín", "Bielorrusia", "Birmania", "Bolivia", "Bosnia y Herzegovina", "Botsuana", "Brasil", "Brunéi", "Bulgaria", "Burkina Faso", "Burundi", "Bután", "Cabo Verde", "Camboya", "Camerún", "Canadá", "Catar", "Chad", "Chile", "China", "Chipre", "Ciudad del Vaticano", "Colombia", "Comoras", "Corea del Norte", "Corea del Sur", "Costa de Marfil", "Costa Rica", "Croacia", "Cuba", "Dinamarca", "Dominica", "Ecuador", "Egipto", "El Salvador", "Emiratos Árabes Unidos", "Eritrea", "Eslovaquia", "Eslovenia", "España", "Estados Unidos", "Estonia", "Etiopía", "Filipinas", "Finlandia", "Fiyi", "Francia", "Gabón", "Gambia", "Georgia", "Ghana", "Granada", "Grecia", "Guatemala", "Guyana", "Guinea", "Guinea ecuatorial", "Guinea-Bisáu", "Haití", "Honduras", "Hungría", "India", "Indonesia", "Irak", "Irán", "Irlanda", "Islandia", "Islas Marshall", "Islas Salomón", "Israel", "Italia", "Jamaica", "Japón", "Jordania", "Kazajistán", "Kenia", "Kirguistán", "Kiribati", "Kuwait", "Laos", "Lesoto", "Letonia", "Líbano", "Liberia", "Libia", "Liechtenstein", "Lituania", "Luxemburgo", "Madagascar", "Malasia", "Malaui", "Maldivas", "Malí", "Malta", "Marruecos", "Mauricio", "Mauritania", "México", "Micronesia", "Moldavia", "Mónaco", "Mongolia", "Montenegro", "Mozambique", "Namibia", "Nauru", "Nepal", "Nicaragua", "Níger", "Nigeria", "Noruega", "Nueva Zelanda", "Omán", "Países Bajos", "Pakistán", "Palaos", "Palestina", "Panamá", "Papúa Nueva Guinea", "Paraguay", "Perú", "Polonia", "Portugal", "Reino Unido", "República Centroafricana", "República Checa", "República de Macedonia", "República del Congo", "República Democrática del Congo", "República Dominicana", "República Sudafricana", "Ruanda", "Rumanía", "Rusia", "Samoa", "San Cristóbal y Nieves", "San Marino", "San Vicente y las Granadinas", "Santa Lucía", "Santo Tomé y Príncipe", "Senegal", "Serbia", "Seychelles", "Sierra Leona", "Singapur", "Siria", "Somalia", "Sri Lanka", "Suazilandia", "Sudán", "Sudán del Sur", "Suecia", "Suiza", "Surinam", "Tailandia", "Tanzania", "Tayikistán", "Timor Oriental", "Togo", "Tonga", "Trinidad y Tobago", "Túnez", "Turkmenistán", "Turquía", "Tuvalu", "Ucrania", "Uganda", "Uruguay", "Uzbekistán", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Yibuti", "Zambia", "Zimbabue"];
@@ -190,6 +191,8 @@ function SingUp(props) {
 
             <div>
 
+                <NavBar />
+
                 <Card maxW='sm'>
 
                     <CardBody>
@@ -234,18 +237,18 @@ function SingUp(props) {
                 <NavBar />
 
                 <br></br>
-                <br></br>
 
-                <WrapItem onClick={() => loginWithRedirect()}>
-                    <Button colorScheme='facebook'>Facebook</Button>
-                </WrapItem>
+                <div>
+
+                    <FacebookLoginButton onClick={() => loginWithRedirect()}></FacebookLoginButton>
+
+                    <br></br>
+
+                    <GoogleLoginButton onClick={() => loginWithRedirect()}></GoogleLoginButton>
+
+                </div>
 
                 <br></br>
-                <br></br>
-
-                <WrapItem onClick={() => loginWithRedirect()}>
-                    <Button colorScheme='gray'>Google</Button>
-                </WrapItem>
 
                 <div className="form">
 
@@ -480,4 +483,4 @@ function SingUp(props) {
 
 };
 
-export default SingUp;
+export default Login;
