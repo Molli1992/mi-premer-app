3751bbe5e5f10655ee509c079d2e6b8aa8c35f77
Tobias Blaksley Iraola <tblanksley@beru.io>
2023-03-15 16:40:30 -0300

    footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
             <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
 
                 <div class="col-md-4 d-flex align-items-center">
-                    <a href="https://github.com/Molli1992?tab=repositories" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                    <a href="https://github.com/Molli1992?tab=repositories" target="_blank" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                         <ion-icon name="logo-github" size="large"></ion-icon>
                     </a>
                     <span class="mb-3 mb-md-0 text-muted">&copy; 2022 Company, Inc</span>
@@ -23,19 +23,19 @@ function Footer() {
                 <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
 
                     <li class="ms-3">
-                        <a class="text-muted" href="https://www.linkedin.com/in/felipe-blaksley-iraola-141b32236/">
+                        <a class="text-muted" href="https://www.linkedin.com/in/felipe-blaksley-iraola-141b32236/" target="_blank">
                             <ion-icon name="logo-linkedin" size="large"></ion-icon>
                         </a>
                     </li>
 
                     <li class="ms-3">
-                        <a class="text-muted" href="https://www.facebook.com/felipeblaksley/">
+                        <a class="text-muted" href="https://www.facebook.com/felipeblaksley/" target="_blank">
                             <ion-icon name="logo-facebook" size="large"></ion-icon>
                         </a>
                     </li>
 
                     <li class="ms-3">
-                        <a class="text-muted" href="#">
+                        <a class="text-muted" href="#" target="_blank">
                             <ion-icon name="logo-google" size="large"></ion-icon>
                         </a>
                     </li>
